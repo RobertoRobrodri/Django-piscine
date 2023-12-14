bba759c7b08c70afa5b862f601300aa62919c2c4
Roberto Rodriguez garcia <robrodri@c2r2s2.42madrid.com>
2023-12-14 17:32:11 +0100

    Finally doing django

--- Python-Django-3/ex02/request_wikipedia.py
+++ Python-Django-3/ex02/request_wikipedia.py
@@ -1,8 +1,21 @@
-import sys, json
+import sys, json, requests, dewiki
 
-def get_wiki_article(arg):
-    print("a")
+def get_wiki_article(subject):
+	wiki_url = 'https://en.wikipedia.org/w/api.php'
+	params = {
+        'action': 'query',
+        'format': 'json',
+        'titles': subject,
+        'prop': 'extracts',
+        'exintro': True,
+        'explaintext': True,
+    }
+
+	r = requests.get(wiki_url, params)
+	article = r.json()
+	page = next(iter(article['query']['pages'].values()))
+	print(page['extract'])
 
 if __name__ == '__main__':
-    if len(sys.argv) == 2:
-        get_wiki_article(sys.argv[1])
+	if len(sys.argv) == 2:
+		get_wiki_article(sys.argv[1])
